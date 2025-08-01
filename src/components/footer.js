fe225d41232daafa70d31d58c7f5d5e2dc960cdc
binaya346 <rijalbinaya@gamil.com>
2025-08-01 16:59:47 +0545

    adds header & footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 My Website</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import "./header.css";
+
+const Header = () => {
+    return (<header>
+        <nav>
+            <ul>
+                <li>
+                    <a href='#'>Home</a>
+                </li>
+                <li>
+                    <a href='#'>Contact</a>
+                </li>
+                <li>
+                    <a href='#'>About</a>
+                </li>
+            </ul>
+        </nav>
+    </header>);
+};
+
+export default Header;
